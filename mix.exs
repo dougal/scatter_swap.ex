bb753070a7466008e8868f3c8fa0c0f6bb0c99fd
Douglas F Shearer <dougal.s@gmail.com>
2017-03-23 21:23:03 +0000

    Update mix.exs for 0.1.0 release.

--- mix.exs
+++ mix.exs
@@ -4,11 +4,12 @@ defmodule ScatterSwap.Mixfile do
   def project do
     [
       app:         :scatter_swap,
-      name:        "ScatterSwap",
+      name:        :scatter_swap,
       version:     "0.1.0",
       elixir:      "~> 1.3",
       source_url:  "https://github.com/dougal/scatter_swap.ex",
       description: "An integer hash function designed to have zero collisions, achieve avalanche, and be reversible.",
+      package:     package(),
       deps:        deps(),
       docs:        docs()
    ]
@@ -29,6 +30,15 @@ defmodule ScatterSwap.Mixfile do
     [
       main:       "readme",
       extras:     ["README.md"]
+    ]
+  end
+
+  defp package do
+    [
+      licenses:    ["MIT"],
+      maintainers: ["Douglas F Shearer"],
+      links:       %{"GitHub" => "https://github.com/dougal/scatter_swap.ex"},
+      files:       ~w(mix.exs README.md CHANGELOG.md LICENCE.txt lib)
     ]
   end
 end
