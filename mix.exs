07b7ee69076e5dc6e26ff279e759e0e1a0b6f745
Douglas F Shearer <dougal.s@gmail.com>
2017-03-23 21:10:20 +0000

    Do not declare support for Elixir < 1.3.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ScatterSwap.Mixfile do
       app:         :scatter_swap,
       name:        "ScatterSwap",
       version:     "0.1.0",
-      elixir:      "~> 1.1",
+      elixir:      "~> 1.3",
       source_url:  "https://github.com/dougal/scatter_swap.ex",
       description: "An integer hash function designed to have zero collisions, achieve avalanche, and be reversible.",
       deps:        deps(),
